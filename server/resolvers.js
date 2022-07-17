4fcce3093646c12e4a9d12a8c7b1a5843336d8f1
Niraj Sah <Nirajsahy4aj@gmail.com>
2022-07-17 13:17:36 +0530

    database-graphql setup

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,17 @@
 const { gql } = require("apollo-server-express");
+const User = require("./models/user.model");
 
 const resolvers = {
   Query: {
-    hello: () => "hi",
+    users: async () => await User.find(),
+  },
+  Mutation: {
+    createUser: async (_, { id, username, email, password }) => {
+      const newUser = new User({ id, username, email, password });
+      await newUser.save();
+      console.log(newUser);
+      return newUser;
+    },
   },
 };
 
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -1,8 +1,24 @@
 const { gql } = require("apollo-server-express");
+const User = require("./models/user.model");
 
 const typeDefs = gql`
   type Query {
     hello: String!
+    users: [User!]!
+  }
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+  }
+  type Mutation {
+    createUser(
+      id: ID!
+      username: String!
+      email: String!
+      password: String!
+    ): User!
   }
 `;
 
