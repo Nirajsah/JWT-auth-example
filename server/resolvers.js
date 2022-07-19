199930bc605a97284bb6dc2cde5243d12ea14d5c
Niraj Sah <Nirajsahy4aj@gmail.com>
2022-07-19 21:23:11 +0530

    CRUD Users

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,5 @@
 const { gql } = require("apollo-server-express");
+const { response } = require("express");
 const User = require("./models/user.model");
 
 const resolvers = {
@@ -11,6 +12,33 @@ const resolvers = {
       await newUser.save();
       console.log(newUser);
       return newUser;
+    },
+    updateUser: async (_, { username, email, password }) => {
+      const userExist = await User.findOne({ email });
+      // console.log(userExist.id);
+      // if (!userExist) {
+      //   console.log("User does not exist");
+      //   return userExist;
+      // }
+      if (userExist.email === email && userExist.password === password) {
+        await User.findByIdAndUpdate(userExist, {
+          username,
+        });
+        const updatedUser = await User.findOne({ email });
+        console.log(updatedUser);
+        return updatedUser;
+      }
+    },
+    deleteUser: async (_, { email, password }) => {
+      const userExist = await User.findOne({ email });
+      if (!userExist) {
+        console.log("User does not exist");
+      }
+      if (userExist.email === email && userExist.password === password) {
+        const deletedUser = await User.findByIdAndDelete(userExist);
+        console.log(deletedUser);
+        return deletedUser;
+      }
     },
   },
 };
